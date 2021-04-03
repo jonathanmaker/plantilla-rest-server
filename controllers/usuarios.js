969ac83d1213f7b3e39d341d1e7fc1d7c55cd23a
jhonix <jonathantbm2015@gmail.com>
2021-04-03 09:32:14 -0300

    respaldo ress api

---
+++ controllers/usuarios.js
@@ -0,0 +1,53 @@
+const { response, request } = require ('express');
+
+
+
+const usuariosGet = (req= request, res = response)=> {
+    const {q, nombre='no name', apikey, page=1, limit}= req.query;
+    res.json({               
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+ }
+
+const usuariosPut = (req, res = response)=> {
+    const {id} = req.params ;
+    res.json({               
+        msg: 'put API',
+        id
+    });
+ }
+ 
+const usuariosPost = (req, res)=> {
+    const { nombre, edad} = req.body;
+    res.json({               
+        msg: 'post API usuariosPost',
+        nombre, 
+        edad 
+    });
+ }
+ 
+const usuariosDelete =  (req, res)=> {
+    res.status(403).json({               
+        msg: 'delete API'
+    });
+ } 
+
+const usuariosPatch = (req, res)=> {
+    res.status(403).json({               
+        msg: 'patch API'
+    });
+ } 
+
+
+ module.exports = {
+     usuariosGet,
+     usuariosPut,
+     usuariosPost,
+     usuariosDelete,
+     usuariosPatch
+ }
